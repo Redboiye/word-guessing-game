d83efd25e61efa87c70d9ac443bdb6c20fb4f058
Kaspars <smarnss@gmail.com>
2024-05-30 20:59:03 +0300

    Move functions to classes

--- classes.py
+++ classes.py
@@ -1,9 +1,6 @@
 from time import sleep
 
-from choose_difficulty import get_difficulty
 import random
-
-from word_functions import display_guessed_character
 
 
 class WordSet:
@@ -30,10 +27,6 @@ class WordSet:
     ]
 
 
-class Player:
-    pass
-
-
 class Match:
     attempts = 10
 
@@ -47,6 +40,15 @@ class Match:
         for character in range(self.random_word_length):
             self.word_hidden_characters.append('_')
 
+    @staticmethod
+    def __display_guessed_character(word_hidden_characters, random_word, player_guess) -> list:
+        """accepts player guessed character, random word that he is guessing, and inserts the character inside
+         it's place in the random word. The function returns the word with guessed and hidden characters."""
+        player_guess_index = random_word.index(player_guess)
+        word_hidden_characters[player_guess_index] = player_guess
+
+        return word_hidden_characters
+
     def start(self, match_is_running):
         print("Time to guess the word, Good luck!")
         while match_is_running == True:
@@ -57,7 +59,7 @@ class Match:
 
             print("your guess : ", player_guess)
             if player_guess in self.random_word:
-                self.word_hidden_characters = display_guessed_character(
+                self.word_hidden_characters = self.__display_guessed_character(
                     self.word_hidden_characters,
                     self.random_word,
                     player_guess,
@@ -90,6 +92,47 @@ class Game:
     game_is_running = True
     match_is_running = False
 
+    @staticmethod
+    def __get_difficulty(words, easy_words, normal_words, hard_words, start_choice):
+        """Player can start choosing the difficult from easy to hard even quit. and function
+        returns the selected difficulty for the word list that is already made, match_is_running is a boolion that
+        makes sure that the player chosen difficulty is True"""
+
+        selected_difficulty = None
+        match_is_running = None
+        hell_mode_word = [["p", "n", "e", "u", "m", "o", "n", "o", "u", "l", "t", "r",
+                           "a", "m", "i", "c", "r", "o", "s", "c", "o", "p", "i",
+                           "c", "s", "i", "l", "i", "c", "o", "v", "o", "l", "c", "a",
+                           "n", "o", "c", "o", "n", "i", "o", "s", "i", "s"]]
+        if start_choice == "q":
+            exit("bye bye")
+        if start_choice == "e":
+            for word in words:
+                if len(word) == 3:
+                    easy_words.append(word)
+            selected_difficulty = easy_words
+            match_is_running = True
+
+        if start_choice == "n":
+            for word in words:
+                if 3 < len(word) <= 7:
+                    normal_words.append(word)
+            selected_difficulty = normal_words
+            match_is_running = True
+        if start_choice == "h":
+            for word in words:
+                if len(word) >= 8:
+                    hard_words.append(word)
+            selected_difficulty = hard_words
+            match_is_running = True
+
+        if start_choice == "666":
+            print("Welcome to HELL MODE!")
+            selected_difficulty = hell_mode_word
+            match_is_running = True
+
+        return selected_difficulty, match_is_running,
+
     def start(self):
         print("Welcome to the word guessing game!")
 
@@ -98,7 +141,7 @@ class Game:
             while self.selected_difficulty is None:
                 start_choice = input("e(easy), n(normal, h(hard), q(quit): ")
 
-                self.selected_difficulty, self.match_is_running = get_difficulty(
+                self.selected_difficulty, self.match_is_running = self.__get_difficulty(
                     WordSet.WORDS,
                     self.easy_words,
                     self.normal_words,
